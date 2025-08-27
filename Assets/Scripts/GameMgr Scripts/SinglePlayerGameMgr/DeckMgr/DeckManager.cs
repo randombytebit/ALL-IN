bc4e3f0723552bf16d588e9a6af246a3777a337f
Leo <cs.lwong4@gmail.com>
2025-08-27 14:34:25 +0800

    build: InitializeDeck, Shuffle, Get, Set method

--- Assets/Scripts/GameMgr Scripts/SinglePlayerGameMgr/DeckMgr/DeckManager.cs
+++ Assets/Scripts/GameMgr Scripts/SinglePlayerGameMgr/DeckMgr/DeckManager.cs
@@ -26,9 +26,15 @@ public class DeckManager : MonoBehaviour
 
     private int _specialCardsCount;
 
-
-    public void InitializeDeck()
+    private void Start()
     {
+        StartCoroutine(InitializeDeck());
+    }
+
+    private IEnumerator InitializeDeck()
+    {
+        yield return null;
+
         foreach (Suits suit in System.Enum.GetValues(typeof(Suits)))
         {
             for (int value = 10; value <= 14; value++)
@@ -36,7 +42,32 @@ public class DeckManager : MonoBehaviour
                 _deck.Add((suit, (CardValue)value));
             }
         }
-        Debug.Log($"Deck initialized with {_deck.Count} cards.");
+        Debug.Log($"Deck initialized with {string.Join(", ", _deck)}");
+    }
+
+    private void ShuffleDeck()
+    {
+        for (int i = 0; i < _deck.Count; i++)
+        {
+            var temp = _deck[i];
+            int randomIndex = Random.Range(0, _deck.Count);
+            _deck[i] = _deck[randomIndex];
+            _deck[randomIndex] = temp;
+        }
+        Debug.Log($"Deck shuffled with {string.Join(", ", _deck)}");
+    }
+
+    public string GetSetOfCards()
+    {
+        if (_deck.Count == 0)
+        {
+            return "[]";
+        }
+    
+        var card = _deck[0];
+        _deck.RemoveAt(0);
+        
+        return $"[{card.suit}, {card.cardValue.ToString().ToLower()}]";
     }
 
     public void InitializeSpecialCards(int numberOfPlayers)
