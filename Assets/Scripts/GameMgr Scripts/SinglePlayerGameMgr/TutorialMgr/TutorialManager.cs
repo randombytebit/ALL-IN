2305d633d18af6e1137ee5146e70759389ed5386
Leo <cs.lwong4@gmail.com>
2025-09-24 14:25:07 +0800

    fix: tutorial manager initalization

--- Assets/Scripts/GameMgr Scripts/SinglePlayerGameMgr/TutorialMgr/TutorialManager.cs
+++ Assets/Scripts/GameMgr Scripts/SinglePlayerGameMgr/TutorialMgr/TutorialManager.cs
@@ -8,6 +8,11 @@ public class TutorialManager : MonoBehaviour
     public async Task Initialize()
     {
         Debug.Log("Tutorial Manager Initialized");
+
+        Application.targetFrameRate = 60;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         await Task.CompletedTask;
     }
 }
